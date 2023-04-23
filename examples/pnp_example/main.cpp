a003fc608b25716541480cdba4b95414cce85220
Bolun Dai <bd1555@nyu.edu>
2023-04-22 23:59:17 -0400

    pnp example template is added

---
+++ examples/pnp_example/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#ifdef USE_OPENCV
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "CVLib.h"
+#endif
+
+
+
+int main(int argc,char ** argv) {
+
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
